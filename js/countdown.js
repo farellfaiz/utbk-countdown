b43a855605385f386a554cf3fce8095b4c1dd022
Farell Faiz <farellfaiz@gmail.com>
2021-02-14 09:11:29 +0700

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,54 @@
+const year = new Date().getFullYear();
+
+const secondMarch = new Date(year, 3, 12).getTime();
+const month = new Date().getMonth();
+
+// countdown
+let timer = setInterval(function() {
+
+    // get today's date
+    const today = new Date().getTime();
+
+    // get the difference
+    let diff;
+    diff = secondMarch - today;
+
+
+
+
+
+    // math
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    // display
+    document.getElementById("timer").innerHTML =
+        "<div class=\"days\"> \
+  <div class=\"numbers\">" + days + "</div>days</div> \
+<div class=\"hours\"> \
+  <div class=\"numbers\">" + hours + "</div>hours</div> \
+<div class=\"minutes\"> \
+  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+<div class=\"seconds\"> \
+  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+</div>";
+
+}, 1000);
+var date = new Date().toISOString().split('T')[0];
+if (date == "2021-03-01") {
+    window.location.href = "https://google.com";
+}
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
