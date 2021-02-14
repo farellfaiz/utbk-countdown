690ad80b3828a969891142c4896a4afe663d82a4
Farell Faiz <farellfaiz@gmail.com>
2021-02-14 15:25:05 +0700

    Add rainToast and libaryToast

--- js/audio.js
+++ js/audio.js
@@ -4,6 +4,22 @@ var librarySound = document.getElementById("library")
 function rain() {
     if (rainSound.paused) {
         rainSound.play();
+        const RainToast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        RainToast.fire({
+            icon: 'success',
+            title: 'Playing rain ambience sound.'
+        })
     } else {
         rainSound.pause();
     }
@@ -12,6 +28,22 @@ function rain() {
 function library() {
     if (librarySound.paused) {
         librarySound.play();
+        const LibraryToast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        LibraryToast.fire({
+            icon: 'success',
+            title: 'Playing library ambience sound.'
+        })
     } else {
         librarySound.pause();
     }
