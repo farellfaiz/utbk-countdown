fffad300592410916bf5b38186fed55486d90be6
Farell Faiz <farellfaiz@gmail.com>
2021-03-04 17:13:40 +0700

    Significantly improve mode toggles performance

---
+++ js/mode.js
@@ -0,0 +1,43 @@
+function dark() {
+    // Main
+    document.documentElement.style.setProperty('--bgbody', '#183059');
+    document.documentElement.style.setProperty('--h1', '#FFFFFF');
+    document.documentElement.style.setProperty('--timer', '#F6F4F3');
+    document.documentElement.style.setProperty('--timeborder', '1px solid rgba(72, 94, 144, 0)');
+    document.documentElement.style.setProperty('--timehover', '#FFFFFF');
+    document.documentElement.style.setProperty('--numbers', '#183059');
+    document.documentElement.style.setProperty('--footertext', '#FFFFFF');
+    document.documentElement.style.setProperty('--footerlink', '#F6F4F3');
+
+    // Bubble
+    document.documentElement.style.setProperty('--bubble1', 'rgba(255, 255, 255, 0.15)');
+    document.documentElement.style.setProperty('--bubble2', 'rgba(255, 255, 255, 0.25)');
+    document.documentElement.style.setProperty('--bubble3', 'rgba(255, 255, 255, 0.2)');
+    document.documentElement.style.setProperty('--bubble4', 'rgba(255, 255, 255, 0.3)');
+
+    // Menu
+    document.documentElement.style.setProperty('--menuprimary', '#FFFFFF');
+    document.documentElement.style.setProperty('--menusecondary', '#183059');
+};
+
+function light() {
+    // Main
+    document.documentElement.style.setProperty('--bgbody', '#FFFFFF');
+    document.documentElement.style.setProperty('--h1', '#707070');
+    document.documentElement.style.setProperty('--timer', '#F6F4F3');
+    document.documentElement.style.setProperty('--timeborder', '1px solid rgba(72, 94, 144, 0.16)');
+    document.documentElement.style.setProperty('--timehover', '#E2EEFF');
+    document.documentElement.style.setProperty('--numbers', '#707070');
+    document.documentElement.style.setProperty('--footertext', '#000000');
+    document.documentElement.style.setProperty('--footerlink', '#87CEEB');
+
+    // Bubble
+    document.documentElement.style.setProperty('--bubble1', 'rgba(34, 122, 255, 0.15)');
+    document.documentElement.style.setProperty('--bubble2', 'rgba(34, 122, 255, 0.15)');
+    document.documentElement.style.setProperty('--bubble3', 'rgba(34, 122, 255, 0.15)');
+    document.documentElement.style.setProperty('--bubble4', 'rgba(34, 122, 255, 0.15)');
+
+    // Menu
+    document.documentElement.style.setProperty('--menuprimary', '#707070 ');
+    document.documentElement.style.setProperty('--menusecondary', '#FFFFFF');
+};
